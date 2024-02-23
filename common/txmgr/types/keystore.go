3dd2e675faceabd3848e46c43c893e56d729c4ab
P-How <peihaoli1014@gmail.com>
2024-02-23 20:55:20 +0100

    added KeyStore interface for managing account keys and subscriptions to key changes

---
+++ common/txmgr/types/keystore.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"github.com/DeAI-Artist/MintAI/common/types"
+)
+
+// KeyStore encompasses the subset of keystore used by txmgr
+//
+//go:generate mockery --quiet --name KeyStore --output ./mocks/ --case=underscore
+type KeyStore[
+	// Account Address type.
+	ADDR types.Hashable,
+	// Chain ID type
+	CHAIN_ID types.ID,
+	// Chain's sequence type. For example, EVM chains use nonce, bitcoin uses UTXO.
+	SEQ types.Sequence,
+] interface {
+	CheckEnabled(address ADDR, chainID CHAIN_ID) error
+	EnabledAddressesForChain(chainId CHAIN_ID) ([]ADDR, error)
+	SubscribeToKeyChanges() (ch chan struct{}, unsub func())
+}
