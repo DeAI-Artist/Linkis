5726913d8de17ecb460e3d26e4fd81b7570cad14
P-How <peihaoli1014@gmail.com>
2024-06-13 21:53:33 +0200

    updated utils tests

--- abci/example/kvstore/txs/tx_utils_test.go
+++ abci/example/kvstore/txs/tx_utils_test.go
@@ -1,7 +1,6 @@
 package txs
 
 import (
-	"encoding/hex"
 	"testing"
 )
 
@@ -9,7 +8,7 @@ func TestHashAndRecover(t *testing.T) {
 	message := []byte("Example `personal_sign` message")
 	expectedAddress := "0x0921e25C5CDd1853c2CF3014D4B0b46d0B2C31ab"
 	sigS := "0x7635254a22cd7762ded328cdb27292884ee2ea21500bbff52ff549b09ef0aaa80dc5e5bc03d67c3ce5176747dcec576bf64d3959bf4a5b0b890fa188026825531b"
-	signature, err := hex.DecodeString(sigS[2:]) // remove the "0x" prefix
+	signature, err := HexToBytes(sigS[2:]) // remove the "0x" prefix
 	if err != nil {
 		t.Fatalf("Failed to decode signature: %s", err)
 	}
