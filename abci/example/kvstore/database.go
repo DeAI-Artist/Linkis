62a95ac1440500391a019ce9b749f60a53bf2c47
P-How <peihaoli1014@gmail.com>
2024-06-14 19:54:32 +0200

    added utils for databased operation

---
+++ abci/example/kvstore/database.go
@@ -0,0 +1,41 @@
+package kvstore
+
+import (
+	"encoding/json"
+	"fmt"
+	db "github.com/tendermint/tm-db"
+)
+
+type ClientInfo struct {
+	Name  string
+	power uint64
+}
+
+type EthereumAddressMap struct {
+	Clients map[string]ClientInfo
+}
+
+// BuildKey generates a database key for a given Ethereum address.
+func BuildKey(ethereumAddress string) []byte {
+	return []byte(fmt.Sprintf("clientRegistration_%s", ethereumAddress))
+}
+
+// StoreClientInfo stores ClientInfo in the database under the key derived from the Ethereum address.
+func StoreClientInfo(db db.DB, ethereumAddress string, info ClientInfo) error {
+	dataBytes, err := json.Marshal(info)
+	if err != nil {
+		return err
+	}
+	return db.Set(BuildKey(ethereumAddress), dataBytes)
+}
+
+// GetClientInfo retrieves ClientInfo from the database using the Ethereum address.
+func GetClientInfo(db db.DB, ethereumAddress string) (ClientInfo, error) {
+	dataBytes, err := db.Get(BuildKey(ethereumAddress))
+	if err != nil {
+		return ClientInfo{}, err
+	}
+	var info ClientInfo
+	err = json.Unmarshal(dataBytes, &info)
+	return info, err
+}
