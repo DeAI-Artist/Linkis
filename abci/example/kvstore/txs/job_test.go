2945228064df6146bc2cba2a2eed502ef2a11b79
P-How <peihaoli1014@gmail.com>
2024-06-22 21:54:56 +0200

    updated miner selection tests typos

--- abci/example/kvstore/txs/job_test.go
+++ abci/example/kvstore/txs/job_test.go
@@ -16,16 +16,16 @@ func TestSelectPseudorandomMiner(t *testing.T) {
 	}
 
 	// Test for consistency: same inputs should always yield the same miner
-	result1 := selectPseudorandomMiner(miners, 100, "abc123", "service1")
-	result2 := selectPseudorandomMiner(miners, 100, "abc123", "service1")
+	result1 := SelectPseudorandomMiner(miners, 100, "abc123", "service1")
+	result2 := SelectPseudorandomMiner(miners, 100, "abc123", "service1")
 	if result1 != result2 {
 		t.Errorf("Expected consistent result but got %s and %s", result1, result2)
 	}
 
 	// Test for variability: different inputs should yield different results
-	result3 := selectPseudorandomMiner(miners, 101, "abc123", "service1") // Changing blockHeight
-	result4 := selectPseudorandomMiner(miners, 100, "abc124", "service1") // Changing appHash
-	result5 := selectPseudorandomMiner(miners, 100, "abc123", "service2") // Changing serviceID
+	result3 := SelectPseudorandomMiner(miners, 101, "abc123", "service1") // Changing blockHeight
+	result4 := SelectPseudorandomMiner(miners, 100, "abc124", "service1") // Changing appHash
+	result5 := SelectPseudorandomMiner(miners, 100, "abc123", "service2") // Changing serviceID
 	//spew.Dump(result3, result4, result5)
 	// Check if any of the results are the same when they shouldn't be
 	if result1 == result3 || result1 == result4 || result1 == result5 {
