4abd470d6de01a494d497b878dece55db543dded
P-How <peihaoli1014@gmail.com>
2024-06-30 18:36:01 +0200

    added default dbDir in checktx.go

--- test/fuzz/mempool/v1/checktx.go
+++ test/fuzz/mempool/v1/checktx.go
@@ -13,7 +13,7 @@ import (
 var mempool mempl.Mempool
 
 func init() {
-	app := kvstore.NewApplication()
+	app := kvstore.NewApplication(config.GetDefaultDBDir())
 	cc := proxy.NewLocalClientCreator(app)
 	appConnMem, _ := cc.NewABCIClient()
 	err := appConnMem.Start()
