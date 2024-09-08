0519fc1e30cb57d983d932e3358f8213d80dde4e
Jackie Lee <33728643+P-HOW@users.noreply.github.com>
2024-09-08 21:08:33 +0200

    Update TokenState.sol
    updated TokenState contracts

--- contracts/contracts/TokenState.sol
+++ contracts/contracts/TokenState.sol
@@ -1,41 +1,61 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.4.20;
 
 // Inheritance
 import "./Owned.sol";
-import "./State.sol";
 
-contract TokenState is Owned, State {
-    /* ERC20 fields. */
+contract TokenState is Owned {
+
+    // the address of the contract that can modify balances and allowances
+    // this can only be changed by the owner of this contract
+    address public associatedContract;
+
+    // ERC20 fields.
     mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(address _owner, address _associatedContract) public Owned(_owner) State(_associatedContract) {}
+    function TokenState(address _owner, address _associatedContract)
+        Owned(_owner)
+        public
+    {
+        associatedContract = _associatedContract;
+        emit AssociatedContractUpdated(_associatedContract);
+    }
 
     /* ========== SETTERS ========== */
 
-    /**
-     * @notice Set ERC20 allowance.
-     * @dev Only the associated contract may call this.
-     * @param tokenOwner The authorising party.
-     * @param spender The authorised party.
-     * @param value The total value the authorised party may spend on the
-     * authorising party's behalf.
-     */
-    function setAllowance(
-        address tokenOwner,
-        address spender,
-        uint value
-    ) external onlyAssociatedContract {
+    // Change the associated contract to a new address
+    function setAssociatedContract(address _associatedContract)
+        external
+        onlyOwner
+    {
+        associatedContract = _associatedContract;
+        emit AssociatedContractUpdated(_associatedContract);
+    }
+
+    function setAllowance(address tokenOwner, address spender, uint value)
+        external
+        onlyAssociatedContract
+    {
         allowance[tokenOwner][spender] = value;
     }
 
-    /**
-     * @notice Set the balance in a given account
-     * @dev Only the associated contract may call this.
-     * @param account The account whose value to set.
-     * @param value The new balance of the given account.
-     */
-    function setBalanceOf(address account, uint value) external onlyAssociatedContract {
+    function setBalanceOf(address account, uint value)
+        external
+        onlyAssociatedContract
+    {
         balanceOf[account] = value;
     }
+
+
+    /* ========== MODIFIERS ========== */
+
+    modifier onlyAssociatedContract
+    {
+        require(msg.sender == associatedContract);
+        _;
+    }
+
+    /* ========== EVENTS ========== */
+
+    event AssociatedContractUpdated(address _associatedContract);
 }
